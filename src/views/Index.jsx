ddc5c54b9b938538a851ae11bb2649bc1a0c96b2
RiadHossain <riadhossain7464@gmail.com>
2021-05-17 00:51:32 +0600

    Update in the problem

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 // reactstrap components
 import {
-  Container,
+  Container
 } from "reactstrap";
 
 import Header from "components/Headers/Header";
-
+import MarkDown from "components/MarkDown/MarkDown";
+import InputArea from "./editor/InputArea";
 const Index = (props) => {
-  
+  let [markdown,setMarkdown] = React.useState('')
+  React.useEffect(()=>setMarkdown(''),[])
   return (
     <>
       <Header />
       <Container fluid>
-        <h3>Hello world</h3>
+        <MarkDown markdown={markdown}/>
+        <InputArea setMarkdown={setMarkdown}/>
       </Container>
     </>
   );
